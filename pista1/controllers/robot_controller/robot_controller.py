118ec0f9c92d5cde8ca8eca441a457441c7bc55d
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-03 01:47:16 -0300

    separar em funções, comentar

--- pista1/controllers/robot_controller/robot_controller.py
+++ pista1/controllers/robot_controller/robot_controller.py
@@ -2,26 +2,34 @@
 
 from controller import Robot
 
-robot = Robot()
-
-timestep = int(robot.getBasicTimeStep())
-
 vel_mult = 3.0
 
+# instanciar robô e obter timestep
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+
+# obter e inicializar motores
 motores = []
 motor_nomes = ['MotorFL', 'MotorFR', 'MotorBL', 'MotorBR']
-
 for nome in motor_nomes:
     device = robot.getDevice(nome)
     device.setPosition(float('inf'))
     device.setVelocity(0.0)
     motores.append(device)
 
-while robot.step(timestep) != -1:
-    velL = 1.0
-    velR = 1.0
 
+def set_velocidade(velL, velR):
     motores[0].setVelocity(velL * vel_mult)
     motores[1].setVelocity(velR * vel_mult)
     motores[2].setVelocity(velL * vel_mult)
     motores[3].setVelocity(velR * vel_mult)
+
+
+def teste():
+    set_velocidade(1.0, 1.0)
+    pass
+
+
+# loop principal
+while robot.step(timestep) != -1:
+    teste()
