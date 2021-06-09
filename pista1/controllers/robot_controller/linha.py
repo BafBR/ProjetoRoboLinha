53a753fb674677e14e8929681566415b8cdc303d
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-09 18:31:27 -0300

    fazer curvas acentuadas
    corrigir colisão da linha
    corrigir lógica de curvas

--- pista1/controllers/robot_controller/linha.py
+++ pista1/controllers/robot_controller/linha.py
@@ -9,51 +9,51 @@ class Linha:
         self.mult_curva = mult_curva
 
     def seguir_linha(self):
-        vel = 1.0, 1.0
-        if self.vejo_linha_left_ext() and not self.achouLinha():
-            print('entrou')
-            vel = 0.0, 1.0
-        elif self.vejo_linha_right_ext() and not self.achouLinha():
-            print('entrou2')
-            while not self.achouLinha():
-                vel = 1.0, 0.0
+        vel = 1.0, 1.0, 0
+        if self.vejo_linha_left_ext():
+            # print('entrou')
+            vel = 0.0, 1.0, 1
+        elif self.vejo_linha_right_ext():
+            # print('entrou2')
+            vel = 1.0, 0.0, -1
         elif self.vejo_linha_left() == self.vejo_linha_right():
-            vel = 1.0, 1.0
+            vel = 1.0, 1.0, 0
         elif self.vejo_linha_left():
-            vel = 0.0, 1.0 
+            vel = 0.0, 1.0, 0
         elif self.vejo_linha_right():
-            vel = 1.0, 0.0
+            vel = 1.0, 0.0, 0
 
         return vel
 
     def vejo_linha_left(self):
         val = self.__vejo_linha(self.ir_l)
-        #print('left ' + str(val))
+        # print('left ' + str(val))
         return val
 
     def vejo_linha_right(self):
         val = self.__vejo_linha(self.ir_r)
-        #print('right ' + str(val))
+        # print('right ' + str(val))
         return val
 
     def vejo_linha_left_ext(self):
-        val = self.__vejo_linha(self.ir_l)
-        #print('left_ext ' + str(val))
+        val = self.__vejo_linha(self.ir_l_ext)
+        # print('left_ext ' + str(val))
         return val
 
     def vejo_linha_right_ext(self):
-        val = self.__vejo_linha(self.ir_r)
-        #print('right_ext ' + str(val))
+        val = self.__vejo_linha(self.ir_r_ext)
+        # print('right_ext ' + str(val))
         return val
-    
+
     def __vejo_linha(self, ir):
         val = ir.getValue()
-        return val < self.ir_thresh
-    
+        # print(val)
+        return val > self.ir_thresh
+
     def achouLinha(self):
         val1 = self.vejo_linha_right()
         val2 = self.vejo_linha_left()
-        print('val1: ' + str(val1) + ' val2: ' + str(val2))
+        # print('val1: ' + str(val1) + ' val2: ' + str(val2))
         if val1:
             return val1
         elif val2:
--- pista1/controllers/robot_controller/robot_controller.py
+++ pista1/controllers/robot_controller/robot_controller.py
@@ -4,9 +4,11 @@ import linha
 
 from controller import Robot
 
-vel_mult = 1.5
-ir_thresh = 270.0
+vel_mult = 3.0
+ir_thresh = 615.0
 mult_curva = 2.0
+
+procurando_linha = 0
 
 # instanciar robô e obter timestep
 robot = Robot()
@@ -24,13 +26,13 @@ for nome in motor_nomes:
 # obter e inicializar sensores IR de linha
     ir_l = robot.getDevice('IR_L')
     ir_l.enable(timestep)
-    
+
     ir_l_ext = robot.getDevice('IR_L_ext')
     ir_l_ext.enable(timestep)
-    
+
     ir_r = robot.getDevice('IR_R')
     ir_r.enable(timestep)
-    
+
     ir_r_ext = robot.getDevice('IR_R_ext')
     ir_r_ext.enable(timestep)
 
@@ -39,6 +41,7 @@ seguelinha = linha.Linha(ir_l, ir_r, ir_l_ext, ir_r_ext, ir_thresh, mult_curva)
 
 
 def set_velocidade(velL, velR):
+    print(velL, velR)
     motores[0].setVelocity(velL * vel_mult)
     motores[1].setVelocity(velR * vel_mult)
     motores[2].setVelocity(velL * vel_mult)
@@ -47,7 +50,13 @@ def set_velocidade(velL, velR):
 
 # loop principal
 while robot.step(timestep) != -1:
-    vel_l, vel_r = seguelinha.seguir_linha()
-    #vel_l, vel_r = 5.0, -5.0
-    print(vel_l, vel_r)
-    set_velocidade(vel_l, vel_r)
+    if procurando_linha > 0:  # procurando na esquerda
+        if seguelinha.vejo_linha_right():
+            procurando_linha = 0
+    elif procurando_linha < 0:  # procurando na direita
+        if seguelinha.vejo_linha_left():
+            procurando_linha = 0
+    else:
+        vel_l, vel_r, dir_procura = seguelinha.seguir_linha()
+        procurando_linha = dir_procura
+        set_velocidade(vel_l, vel_r)
